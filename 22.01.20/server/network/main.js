c23ba9674759a215e3d0341930b807044daefc4a
jeonjinoung <89626182+jeonjinoung@users.noreply.github.com>
2022-02-07 12:19:44 +0900

    feat : 새로운 시작

--- 22.01.20/server/network/main.js
+++ 22.01.20/server/network/main.js
@@ -2,12 +2,18 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const {Blocks, getBlocks} = require('../blockchain/blocks')
+const { getVersion } = require('../utils/isValidBlock')
 
 app.use(express.json())
 
 app.get('/blocks', (req,res)=>{
     res.send(getBlocks())
 })
+
+app.get('/version', (req,res)=>{
+    res.send(getVersion());
+})
+
 
 app.listen(port,()=>{
     console.log(`server start port ${port}`)
@@ -19,4 +25,11 @@ app.listen(port,()=>{
 peer / 간단한 기록들 / 버전
 중단 
 */
+
+//블럭읽어오기
+//curl -X GET http://localhost:3000/block
+
+//블럭 파이썬 제이슨형태로 블럭을 읽어옴
+//curl -X GET http://localhost:3000/blocks python -m json.tool
+
 
