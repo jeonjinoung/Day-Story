8f9875bd1be790003a20413d0d61122256b3dd63
jeonjinoung <89626182+jeonjinoung@users.noreply.github.com>
2022-03-29 09:11:36 +0900

    MAOUS ERROR

--- 22.03.28/client/src/components/main/Main.jsx
+++ 22.03.28/client/src/components/main/Main.jsx
@@ -15,56 +15,70 @@ import { Playlist } from "./mypage/playlist/Playlist";
 import { Collection } from "./mypage/collection/Collection";
 import { History } from "./mypage/history/History";
 
+import axios from "axios";
+
 export const Main = () => {
-	const [address, setAddress] = useState("");
-	async function init () {
-		await Metamask.getAccounts(setAddress);
-		await Metamask.walletListener(setAddress);
-	} 
+  const [address, setAddress] = useState("");
+  const [guest, setGuest] = useState("");
 
-	useEffect(() => {
-		init()
-		const sidebarToggle = document.querySelector(".sidebar-toggle");
-		const sidebar = document.querySelector("nav");
+  async function init() {
+    await Metamask.getAccounts(setAddress);
+    await Metamask.walletListener(setAddress);
+    return address;
+  }
 
-		let getMenuStatus = localStorage.getItem("menu_status");
-		if (getMenuStatus && getMenuStatus === "close") {
-			sidebar.classList.toggle("close");
-		}
+  async function User() {
+    const url = "http://localhost:5000/users/signin";
+    const response = await axios.post(url, { address });
+    // console.log(response.data);
+  }
 
-		sidebarToggle.addEventListener("click", () => {
-			sidebar.classList.toggle("close");
-			if (sidebar.classList.contains("close")) {
-				localStorage.setItem("menu_status", "close");
-			} else {
-				localStorage.setItem("menu_status", "open");
-			}
-		});
+  useEffect(() => {
+    const Useraddress = it();
+    console.log(Useraddress);
+    User();
+    const sidebarToggle = document.querySelector(".sidebar-toggle");
+    const sidebar = document.querySelector("nav");
 
-		
-	}, []);
+    let getMenuStatus = localStorage.getItem("menu_status");
+    if (getMenuStatus && getMenuStatus === "close") {
+      sidebar.classList.toggle("close");
+    }
 
-	return (
-		<section className="main">
-			<Searchbar address={address}/>
-			<div className="main-content"> 
-				<Routes>
-					<Route path="/">
-						<Route index element={<Dashboard />} />
-						<Route path="mypage" element={<Mypage address={address} />}>
-							<Route path="favorite" element={<Favorite address={address} />} />
-							<Route path="playlist" element={<Playlist address={address} />} />
-							<Route path="collection" element={<Collection address={address} />} />
-							<Route path="history" element={<History address={address} />} />
-						</Route>
-						<Route path="music" element={<Music />} />
-						<Route path="store" element={<Store />} />
-						<Route path="auction" element={<Auction />} />
-						<Route path="artist" element={<Artist />} />
-					</Route>
-				</Routes>
-			</div>
-			<Playbar/>
-		</section>
-	);
+    sidebarToggle.addEventListener("click", () => {
+      sidebar.classList.toggle("close");
+      if (sidebar.classList.contains("close")) {
+        localStorage.setItem("menu_status", "close");
+      } else {
+        localStorage.setItem("menu_status", "open");
+      }
+    });
+  }, []);
+
+  return (
+    <section className="main">
+      <Searchbar address={address} />
+      <div className="main-content">
+        <Routes>
+          <Route path="/">
+            <Route index element={<Dashboard />} />
+            <Route path="mypage" element={<Mypage address={address} />}>
+              <Route path="favorite" element={<Favorite address={address} />} />
+              <Route path="playlist" element={<Playlist address={address} />} />
+              <Route
+                path="collection"
+                element={<Collection address={address} />}
+              />
+              <Route path="history" element={<History address={address} />} />
+            </Route>
+            <Route path="music" element={<Music />} />
+            <Route path="store" element={<Store />} />
+            <Route path="auction" element={<Auction />} />
+            <Route path="artist" element={<Artist />} />
+          </Route>
+        </Routes>
+      </div>
+      <Playbar />
+    </section>
+  );
 };
